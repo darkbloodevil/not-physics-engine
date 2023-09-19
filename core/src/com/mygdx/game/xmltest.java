8b8b5b4d4fbc0ea1c1abb6540eff69a7c59768a3
chongkai <chongkai@linewell.com>
2023-09-19 18:45:58 +0800

    xml

--- core/src/com/mygdx/game/xmltest.java
+++ core/src/com/mygdx/game/xmltest.java
@@ -1,9 +1,11 @@
 package com.mygdx.game;
 
+import java.io.File;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 import org.dom4j.*;
 import org.dom4j.tree.DefaultDocument;
@@ -16,17 +18,37 @@ public class xmltest {
         ArrayList<JSONObject> bookList = new ArrayList<JSONObject>();
         SAXReader reader = new SAXReader();
         try {
-            Document document = reader.read(new InputSource( new StringReader( """
-                    <?xml version="1.0" encoding="UTF-8"?>
-                    <note>
-                      <to>Tove</to>
-                      <from>Jani</from>
-                      <heading>Reminder</heading>
-                      <body>Don't forget me this weekend!</body>
-                    </note>""")));
+//            Document document = reader.read(new InputSource( new StringReader( """
+//                    <?xml version="1.0" encoding="UTF-8"?>
+//                    <note>
+//                      <force>
+//                      </force>
+//                      <from>Jani</from>
+//                      <heading>Reminder</heading>
+//                      <body>Don't forget me this weekend!</body>
+//                    </note>""")));
+//
+//            Document document1= DocumentHelper.createDocument();
+//            Element root = document1.addElement("root");
+            Document document= reader.read(new File("test.xml"));
+            Element root_element=document.getRootElement();
+            Stack<Iterator<Element>> iterator_stack=new Stack<>();
+            iterator_stack.add(root_element.elementIterator());
+            while (!iterator_stack.empty()){
+                Iterator<Element> iterator=iterator_stack.peek();
+                if (!iterator.hasNext()){
+                    iterator_stack.pop();
+                    continue;
+                }
+                Element element=iterator.next();
+                if (element.elementIterator().hasNext()){
+                    iterator_stack.push(element.elementIterator());
+                    System.out.println(element.getName()+"\t");
+                }else {
+                    System.out.println(element.getName()+"\t"+element.getText());
+                }
 
-            Document document1= DocumentHelper.createDocument();
-            Element root = document1.addElement("root");
+            }
 
 
         } catch (DocumentException e) {
