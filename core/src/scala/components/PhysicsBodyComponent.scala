b064846ddb916dd1d01e0c474993b77875c3ed40
chongkai <chongkai@linewell.com>
2023-10-16 16:40:40 +0800

    contact

--- core/src/scala/components/PhysicsBodyComponent.scala
+++ core/src/scala/components/PhysicsBodyComponent.scala
@@ -5,8 +5,24 @@ import com.badlogic.gdx.physics.box2d.Body
 
 /**
  * 用来记录body的component（就是为了用上component）
+ *
  */
-class PhysicsBodyComponent extends Component {
+class PhysicsBodyComponent() extends Component {
     var body: Body = _
     var body_id: Long = 0
+
+    /**
+     * 用于表示物理系统的状态（这样子如果同时触发多个碰撞事件，就不会导致覆盖。
+     */
+    var physics_system_state: Any = _
+
+    /**
+     * 上次发生碰撞的时间
+     */
+    var last_contact:Float=0f
+
+    /**
+     * 用于表示碰撞检测的时间间隔（比如说撞一下，不可能触发多次判定）
+     */
+    var contact_interval:Float=0.05f
 }
--- core/src/scala/systems/PhysicsSystem.scala
+++ core/src/scala/systems/PhysicsSystem.scala
@@ -9,7 +9,6 @@ import com.badlogic.gdx.utils.{Array => GDXarray}
 import components.{IdontKnowComponent, PhysicsBodyComponent, TransformComponent}
 
 
-
 class PhysicsSystem(var engine: Engine) extends EntitySystem {
     var PHYSICS_WORLD: World = _
     private val TIME_STEP = .005f
@@ -18,6 +17,7 @@ class PhysicsSystem(var engine: Engine) extends EntitySystem {
     private var physicsBodyMapper: ComponentMapper[PhysicsBodyComponent] = _
     private var transformMapper: ComponentMapper[TransformComponent] = _
     private var idkMapper: ComponentMapper[IdontKnowComponent] = _
+    private var world_state = System.currentTimeMillis()
 
     var GRAVITY = 10
     private var accumulator: Float = 0
@@ -25,21 +25,34 @@ class PhysicsSystem(var engine: Engine) extends EntitySystem {
     physicsBodyMapper = ComponentMapper.getFor(classOf[PhysicsBodyComponent])
     transformMapper = ComponentMapper.getFor(classOf[TransformComponent])
     idkMapper = ComponentMapper.getFor(classOf[IdontKnowComponent])
-    def set_world(world: World): Unit ={
-        PHYSICS_WORLD=world
+
+    def set_world(world: World): Unit = {
+        PHYSICS_WORLD = world
         PHYSICS_WORLD.setContactListener(new ContactListener() {
             override def beginContact(contact: Contact): Unit = {
+                println("contact!")
                 val entity1: Entity = contact.getFixtureA.getBody.getUserData.asInstanceOf[Entity]
-                val entity2: Entity = contact.getFixtureA.getBody.getUserData.asInstanceOf[Entity]
-                val idk1:Option[IdontKnowComponent] =Option(idkMapper.get(entity1))
-                val idk2:Option[IdontKnowComponent] =Option(idkMapper.get(entity2))
-                if(idk2.isDefined){
-                    idk2.get.x_v = (2*(scala.util.Random.nextInt(1)-0.5)).toInt*idk2.get.y_v+scala.util.Random.nextInt(3)
-                    idk2.get.y_v = (2*(scala.util.Random.nextInt(1)-0.5)).toInt*idk2.get.x_v+scala.util.Random.nextInt(1)
+                val entity2: Entity = contact.getFixtureB.getBody.getUserData.asInstanceOf[Entity]
+                val idk1: Option[IdontKnowComponent] = Option(idkMapper.get(entity1))
+                val idk2: Option[IdontKnowComponent] = Option(idkMapper.get(entity2))
+                val pbc1 = physicsBodyMapper.get(entity1)
+                val pbc2 = physicsBodyMapper.get(entity1)
+                if (idk2.isDefined) {
+                    println("2")
+                    if(pbc2.last_contact>0)
+                        return
+                    pbc2.last_contact=pbc2.contact_interval
+                    println(pbc2.last_contact)
+                    idk2.get.x_v = (2 * (scala.util.Random.nextInt(10) - 5)).toInt
+                    idk2.get.y_v = -idk2.get.y_v
                 }
-                if(idk1.isDefined){
-                    idk1.get.x_v = (2*(scala.util.Random.nextInt(1)-0.5)).toInt*idk1.get.y_v+scala.util.Random.nextInt(5)
-                    idk1.get.y_v = (2*(scala.util.Random.nextInt(1)-0.5)).toInt*idk1.get.x_v+scala.util.Random.nextInt(5)
+                if (idk1.isDefined) {
+                    println("1")
+                    if(pbc1.last_contact>0)
+                        return
+                    pbc1.last_contact=pbc1.contact_interval
+                    idk1.get.x_v = (2 * (scala.util.Random.nextInt(1) - 0.5)).toInt * idk1.get.y_v
+                    idk1.get.y_v = (2 * (scala.util.Random.nextInt(1) - 0.5)).toInt * idk1.get.x_v
                 }
             }
 
@@ -62,6 +75,7 @@ class PhysicsSystem(var engine: Engine) extends EntitySystem {
 
     override def update(deltaTime: Float): Unit = {
         super.update(deltaTime)
+        world_state = System.currentTimeMillis()
         // fixed time step
         // max frame time to avoid spiral of death (on slow devices)
         val frameTime = Math.min(deltaTime, 0.25f)
@@ -74,35 +88,55 @@ class PhysicsSystem(var engine: Engine) extends EntitySystem {
             //PHYSICS_WORLD.step(deltaTime, 2, 2);
             accumulator -= TIME_STEP
         }
-        update_bodies()
+        update_bodies(deltaTime)
     }
 
     /**
      * 处理运动的原动力
      * 并且与显示上的同步
      */
-    def update_bodies(): Unit = {
+    def update_bodies(deltaTime: Float): Unit = {
         val bodies = new GDXarray[Body];
         PHYSICS_WORLD.getBodies(bodies)
         for (body <- bodies.items) {
 
             val body_entity: Entity = body.getUserData.asInstanceOf[Entity]
-            if (body_entity != null) {
+
+            if(body_entity != null){
                 val idk: IdontKnowComponent = idkMapper.get(body_entity)
-                if (idk != null) {
-                    body.setLinearVelocity(new Vector2(idk.x_v.toFloat, idk.y_v.toFloat))
-                }
-            }
+                if(idk != null)body.setLinearVelocity(new Vector2(idk.x_v.toFloat, idk.y_v.toFloat))
 
-            val tc = transformMapper.get(body_entity)
-            if (tc != null) { // Update the entities/sprites position and angle
-                tc.pos.x = body.getPosition.x
-                tc.pos.y = body.getPosition.y
-                // We need to convert our angle from radians to degrees
-                tc.rotation = body.getAngle
-                //Gdx.app.log("my","position "+b.getPosition().x+" "+ b.getPosition().y);
-            }
+                update_transform(body_entity,body)
+                update_last_contact(body_entity,deltaTime)
 
+            }
+        }
+    }
+    def update_transform(body_entity:Entity,body: Body): Unit ={
+        val tc = transformMapper.get(body_entity)
+        if(tc != null) // Update the entities/sprites position and angle
+        {
+            tc.pos.x = body.getPosition.x
+            tc.pos.y = body.getPosition.y
+            // We need to convert our angle from radians to degrees
+            tc.rotation = body.getAngle
+            //Gdx.app.log("my","position "+b.getPosition().x+" "+ b.getPosition().y);
+        }
+    }
+
+    def update_last_contact(body_entity:Entity,deltaTime:Float): Unit ={
+        /**
+         * 碰撞终止时间
+         */
+        val pbc = physicsBodyMapper.get(body_entity)
+
+        pbc.physics_system_state=world_state
+        if(pbc.last_contact>0) {
+
+            pbc.last_contact -= deltaTime
+            if (pbc.last_contact<=0){
+                pbc.last_contact=0
+            }
         }
     }
 
