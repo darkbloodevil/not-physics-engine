e060a66b4de38673c0cb95a37377260294fc4b63
darkbloodevil <2573283827@qq.com>
2023-12-19 17:40:21 +0800

    修改MessageProcessingSystem的entity处理方式

--- core/src/java/systems/MessageProcessingSystem.java
+++ core/src/java/systems/MessageProcessingSystem.java
@@ -12,24 +12,30 @@ import org.slf4j.LoggerFactory;
 /**
  * 统一处理msg
  */
-public class MessageProcessingSystem extends EntityProcessingSystem {
+public class MessageProcessingSystem extends EntitySystem {
     public static String MESSAGE_PROCESSING_SYSTEM_TOKEN = "message processing system";
+
+    Logger logger = LoggerFactory.getLogger(MessageProcessingSystem.class);
+
     ComponentMapper<GameMsgComponent> gameMsgMapper;
     ComponentMapper<PropertyComponent> propertyMapper;
-    Logger logger = LoggerFactory.getLogger(MessageProcessingSystem.class);
 
     public MessageProcessingSystem(GameWorld gameWorld) {
         super(Aspect.all(GameMsgComponent.class, PropertyComponent.class));
+
     }
 
     @Override
-    protected void process(Entity entity) {
+    protected void processSystem() {
+        this.getEntities().forEach(this::process_single);
+    }
+
+    protected void process_single(Entity entity) {
 
         GameMsgComponent gameMsgComponent = gameMsgMapper.get(entity);
         PropertyComponent propertyComponent = propertyMapper.get(entity);
         JSONObject msg = gameMsgComponent.msg();
         JSONObject property = propertyComponent.property();
-        
         if (msg.has(EventEnum.CONTACT.toString()) && property.has("name")) {
             String contact_tar = msg.getString(EventEnum.CONTACT.toString());
             String name = property.getString("name");
@@ -37,10 +43,9 @@ public class MessageProcessingSystem extends EntityProcessingSystem {
             if (name.equals("big")) {
                 if (contact_tar.equals("wall")) {
                     var physicsInstruction = entity.edit().create(PhysicsInstruction.class);
-                    
                     physicsInstruction.instructions().put("CHANGE_DIRECTION");
                 } else if (contact_tar.equals("big")) {
-//                    var instruction_str = " {\"name\":\"small\",\"position\":%s} ";
+                    //                    var instruction_str = " {\"name\":\"small\",\"position\":%s} ";
                     var instruction_str = """
                             {"name":"small","position":%s}
                             """;
@@ -54,3 +59,37 @@ public class MessageProcessingSystem extends EntityProcessingSystem {
 
     }
 }
+/*
+
+      @Override
+ *     protected void process(Entity entity) {
+ *
+ *         GameMsgComponent gameMsgComponent = gameMsgMapper.get(entity);
+ *         PropertyComponent propertyComponent = propertyMapper.get(entity);
+ *         JSONObject msg = gameMsgComponent.msg();
+ *         JSONObject property = propertyComponent.property();
+ *
+ *         if (msg.has(EventEnum.CONTACT.toString()) && property.has("name")) {
+ *             String contact_tar = msg.getString(EventEnum.CONTACT.toString());
+ *             String name = property.getString("name");
+ *             //
+ *             if (name.equals("big")) {
+ *                 if (contact_tar.equals("wall")) {
+ *                     var physicsInstruction = entity.edit().create(PhysicsInstruction.class);
+ *
+ *                     physicsInstruction.instructions().put("CHANGE_DIRECTION");
+ *                 } else if (contact_tar.equals("big")) {
+ * //                    var instruction_str = " {\"name\":\"small\",\"position\":%s} ";
+ *                     var instruction_str = """
+ *                             {"name":"small","position":%s}
+ *                             """;
+ *                     EntityGenerator.add_instruction(this,
+ *                             new JSONObject(String.format(instruction_str,
+ *                                     msg.getJSONObject("position").toString())));
+ *                 }
+ *             }
+ *
+ *         }
+ *
+ *     }
+ */
