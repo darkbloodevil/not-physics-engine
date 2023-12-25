a83b2b80b9177e9ce12cc9be543c70b1f5e7f78a
darkbloodevil <2573283827@qq.com>
2023-12-25 17:20:36 +0800

    用于测试的系统指令类

---
+++ core/test/java/systems/SystemInstructionT.java
@@ -0,0 +1,24 @@
+package systems;
+
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.systems.EntityProcessingSystem;
+import components.InstructionComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SystemInstructionT extends EntityProcessingSystem {
+    ComponentMapper<InstructionComponent> instructionMapper;
+    Logger logger = LoggerFactory.getLogger(SystemInstructionT.class);
+    public SystemInstructionT(){
+        Aspect.all(InstructionComponent.class);
+    }
+
+    @Override
+    protected void process(Entity entity) {
+        logger.info(instructionMapper.get(entity).instructions().toString());
+        instructionMapper.remove(entity);
+    }
+
+}
