81c35136654743c86508322a35d2239cba0a9ea2
darkbloodevil <2573283827@qq.com>
2023-12-29 14:57:28 +0800

    TabularToMap没理由作为独立文件（因为各种模式都要写自身的适配无法通用）

--- core/src/java/NotBox2D/GameWorld.java
+++ core/src/java/NotBox2D/GameWorld.java
@@ -18,6 +18,7 @@ import systems.PhysicsSystem;
 import tools.DeltaTimeRecorder;
 import tools.ExcelReader;
 import tools.IdGenerator;
+import tools.JsonReader;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -184,4 +185,47 @@ public class GameWorld {
         return body;
     }
 
+}
+/**
+ * @author darkbloodevil
+ * 表格转成实际的地图
+ */
+enum TabularToMap {
+    INSTANCE;
+    public float interval;
+    public float center_x=8,center_y=4.5f,offset_x=-0.5f,offset_y=-0.5f;
+    public static final String EMPTY="[EMPTY]";
+
+    TabularToMap(){
+        interval=1;
+    }
+
+    /**
+     * 将表格画成地图
+     * @param tabular
+     * @param representation
+     * @param alterMap
+     * @param gameWorld
+     */
+    public void tabular_to_map(String[][] tabular, HashMap<String,String> representation,
+                               HashMap<String,JSONObject> alterMap,GameWorld gameWorld){
+        for (int j = 0; j < tabular.length; j++) {
+            for (int i = 0; i < tabular[j].length; i++) {
+                //获取表格tag对应的实体名
+                String tag=tabular[tabular.length-j-1][i];
+                if (tag.equals(EMPTY))continue;// 空就直接过
+                String body_name=representation.get(tag);
+                // 获取对应的位置
+                float x=(i-offset_x)*interval-center_x;
+                float y=(j-offset_y)*interval-center_y;
+                //在原型上给予对应的改变量
+                JSONObject alter=new JSONObject("{\"position\":["+x+","+y+"]}");
+                if (alterMap.containsKey(tag)){
+                    alter= JsonReader.merge_json(alter,alterMap.get(tag));
+                }
+                gameWorld.getAlteredBody(body_name,alter);
+
+            }
+        }
+    }
 }
