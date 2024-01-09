fdc8346f710c96d68327d653667712c92fc2dcb2
darkbloodevil <2573283827@qq.com>
2024-01-09 14:31:50 +0800

    cmd组件

--- core/src/java/game/NotPhysicsEngineCLI.java
+++ core/src/java/game/NotPhysicsEngineCLI.java
@@ -1,17 +1,11 @@
 package game;
 
-import components.GameMsgComponent;
-
 import java.awt.*;
 
-import org.apache.commons.lang3.StringUtils;
-import tools.AnsiWrapper;
+import tools.cmdLine.AnsiWrapper;
+import tools.cmdLine.CmdCanvas;
 
 import java.awt.image.BufferedImage;
-import java.io.PrintStream;
-import java.util.Scanner;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class NotPhysicsEngineCLI {
     // ANSI颜色代码
@@ -30,37 +24,42 @@ public class NotPhysicsEngineCLI {
         System.out.println(RED + " two heart ♥♥ is ok" + RESET);
         System.out.println(BOLD_RED + " two heart ♥♥ is ok" + RESET);
         System.out.println(PURPLE + " one heart ♥ is not" + RESET);
+
+        CmdCanvas cc=new CmdCanvas(128,64);
+//        cc.graphics2D().drawRect(3,3,10,5);
+        cc.render();
+        
 //        System.out.format("%-10s %-10s %-10s%n","ha","wa","ka");
 //        System.out.format("   %-10s %-10s %-10s%n","ha","wa","ka");
 
         // https://stackoverflow.com/questions/7098972/ascii-art-java
-        BufferedImage image = new BufferedImage(144, 32, BufferedImage.TYPE_INT_RGB);
-        Graphics graphics = image.getGraphics();
-        graphics.setFont(new Font("Dialog", Font.ITALIC, 14));
-        Graphics2D graphics2D = (Graphics2D) graphics;
-        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics2D.drawString(String.join(" ", new String[]{"Hello,", "world"}), 6, 24);
-        graphics2D.drawRect(0, 12, 72, 20);
-        String ha = "你好";
-        AnsiWrapper ansiWrapper = new AnsiWrapper(AnsiWrapper.ITALIC(),new AnsiWrapper(AnsiWrapper.BG_MAGENTA()));
-        for (int y = 0; y < 32; y++) {
-            StringBuilder builder = new StringBuilder();
-            for (int x = 0; x < 144; x++)
-                builder.append(image.getRGB(x, y) == -16777216 ? " " : image.getRGB(x, y) == -1 ? "#" : "*");
-
-            if (y == 16 || y == 17) {
-
-                int verbose_length = 8;
-                builder.replace(16, 17 + verbose_length, ("%-" + verbose_length + "s").formatted(ha));
-            }
-            if (builder.toString().trim().isEmpty()) continue;
-            var result = builder.toString();
-            if (y == 16) {
-                result = ansiWrapper.wrap(result, ha, true);
-            }
-            System.out.println(result);
-        }
+//        BufferedImage image = new BufferedImage(144, 32, BufferedImage.TYPE_INT_RGB);
+//        Graphics graphics = image.getGraphics();
+//        graphics.setFont(new Font("Dialog", Font.ITALIC, 14));
+//        Graphics2D graphics2D = (Graphics2D) graphics;
+//        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+//                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//        graphics2D.drawString(String.join(" ", new String[]{"Hello,", "world"}), 6, 24);
+//        graphics2D.drawRect(0, 12, 72, 20);
+//        String ha = "你好";
+//        AnsiWrapper ansiWrapper = new AnsiWrapper(AnsiWrapper.ITALIC(),new AnsiWrapper(AnsiWrapper.BG_MAGENTA()));
+//        for (int y = 0; y < 32; y++) {
+//            StringBuilder builder = new StringBuilder();
+//            for (int x = 0; x < 144; x++)
+//                builder.append(image.getRGB(x, y) == -16777216 ? " " : image.getRGB(x, y) == -1 ? "#" : "*");
+//
+//            if (y == 16 || y == 17) {
+//
+//                int verbose_length = 8;
+//                builder.replace(16, 17 + verbose_length, ("%-" + verbose_length + "s").formatted(ha));
+//            }
+//            if (builder.toString().trim().isEmpty()) continue;
+//            var result = builder.toString();
+//            if (y == 16) {
+//                result = ansiWrapper.wrap(result, ha, true);
+//            }
+//            System.out.println(result);
+//        }
 
     }
 }
