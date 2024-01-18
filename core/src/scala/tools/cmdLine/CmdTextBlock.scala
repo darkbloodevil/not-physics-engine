8a82f90d25ef7d15f38c87258065035ccd0a504a
darkbloodevil <2573283827@qq.com>
2024-01-18 17:07:29 +0800

    cmd组件

--- core/src/scala/tools/cmdLine/CmdTextBlock.scala
+++ core/src/scala/tools/cmdLine/CmdTextBlock.scala
@@ -2,15 +2,33 @@ package tools.cmdLine
 
 /**
  * 命令行文本块的类
- * 
+ *
  * 形如
  * ||====================||
  * ||名称：darkbloodevil  ||
  * ||描述：not-physics-   ||
  * ||-engine的开发者      ||
  * ||===================||
- * 
+ *
  */
-class CmdTextBlock {
+class CmdTextBlock(width: Int, height: Int) extends CmdRender {
+    /**
+     * 这个text block有什么properties
+     */
+    var properties: Map[String, Any] = Map()
+    /**
+     * 某个key的颜色
+     */
+    var key_ansi: Map[String, AnsiWrapper] = Map()
+    /**
+     * 某个key对应的value的颜色
+     */
+    var value_ansi: Map[String, AnsiWrapper] = Map()
     
+    override def render(builders: IndexedSeq[StringBuilder]): Unit = {
+
+    }
+    
+    
+
 }
