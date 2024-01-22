79c513855c7eb1307e9665643c5776cf35645b65
darkbloodevil <2573283827@qq.com>
2024-01-22 17:09:08 +0800

    image

--- core/src/scala/tools/cmdLine/CmdImage.scala
+++ core/src/scala/tools/cmdLine/CmdImage.scala
@@ -1,13 +1,15 @@
 package tools.cmdLine
 
 import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import java.io.File
 
 class CmdImage(imagePath: String) {
     // 读取图片
     private val temp_image: BufferedImage = ImageIO.read(new File(imagePath))
     // 获取图片宽度和高度
-    private val image_width: int = temp_image.getWidth
-    private val image_height: int = temp_image.getHeight
+    private val image_width: Int = temp_image.getWidth
+    private val image_height: Int = temp_image.getHeight
     
     
     private val builders = for _ <- 0 until image_height yield StringBuilder()
@@ -16,7 +18,7 @@ class CmdImage(imagePath: String) {
         // 输出各个点的像素值
         for (y <- 0 until image_height) {
             val builder = builders.apply(y)
-            for (x <- 0 until width) {
+            for (x <- 0 until image_width) {
                 if (x * 2 < temp_image.getWidth && y * 4 < temp_image.getWidth) {
                     val pixel = temp_image.getRGB(x, y * 4)
                     // 分离RGB值
